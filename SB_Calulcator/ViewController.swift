309844c4cd773f40f4942638c32e25f51e0d01a7
VM_Mac <vm_mac@VM-Macs-iMac.local>
2018-11-01 07:22:43 -0700

    Initial Commit

---
+++ SB_Calulcator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SB_Calulcator
+//
+//  Created by VM_Mac on 01/11/2018.
+//  Copyright © 2018 VM_Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SB_CalulcatorTests/SB_CalulcatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  SB_CalulcatorTests.swift
+//  SB_CalulcatorTests
+//
+//  Created by VM_Mac on 01/11/2018.
+//  Copyright © 2018 VM_Mac. All rights reserved.
+//
+
+import XCTest
+@testable import SB_Calulcator
+
+class SB_CalulcatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
